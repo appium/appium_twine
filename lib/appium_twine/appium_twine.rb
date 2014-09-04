d6117d5a4033ca6355365d68beb93c9d317c70b7
bootstraponline <code@bootstraponline.com>
2014-09-04 14:19:39 -0400

    Use new format array

--- lib/appium_twine/appium_twine.rb
+++ lib/appium_twine/appium_twine.rb
@@ -4,25 +4,4 @@ require 'twine'
 require_relative 'formatter'
 require_relative 'version'
 
-module Appium
-  module Twine
-    class << self
-      # Adds a single formatter to the Twine::Formatters constant.
-      #
-      # @param formatter [Class] new formatter to append. Example: Formatters::Foobar
-      def add_formatter formatter
-        target_module   = ::Twine::Formatters
-        target_constant = :FORMATTERS
-
-        return unless target_module.const_defined? target_constant
-
-        old_value = target_module.send :remove_const, target_constant
-        new_value = old_value << formatter
-
-        target_module.send :const_set, target_constant, new_value
-      end
-    end
-  end
-end
-
-Appium::Twine.add_formatter Twine::Formatters::Csharp
+Twine::Formatters.formatters += [Twine::Formatters::Csharp]
